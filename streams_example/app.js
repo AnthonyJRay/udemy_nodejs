6464737fe1f7d5fcb0fba726a494112449c98874
AnthonyJRay <ajrayid@gmail.com>
2022-11-22 18:44:22 -0600

    Notes. Pipes

--- streams_example/app.js
+++ streams_example/app.js
@@ -1,8 +1,11 @@
+// This will create a "readable stream" to read the contents on greet.txt and write that text through a "writable stream" in greetCopy.txt
+// Play with the buffer size to experiment with stream and chunks operating asynchronously.
+
 var fs = require('fs');
 
 var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 1024});
 
-var writable = fs.createWriteStream(__dirname + '/greetCopy.txt');
+var writable = fs.createWriteStream(__dirname + '/greetCopy.md');
 
 readable.on('data', function(chunk)  {
     console.log(chunk.length);
@@ -16,5 +19,4 @@ readable.on('data', function(chunk)  {
 // The "highWaterMark" options allows you to set a buffer size.
 
 
-// The current greet.txt file is roughly 3188 bytes. Setting the buffer to 1024 bytes, it will send out data "chucks" each time it fills 1024 bytes.
-// This results in the buffer sending 4 chunks. 1024, 1024, 1024, 116.
+// The current greet.txt file is roughly 62,000 bytes. Setting the buffer to 1024 bytes, it will send out data "chucks" each time it fills 1024 bytes.
